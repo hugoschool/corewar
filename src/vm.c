b9c8d615495b39269efe1b01c70e7862ce4b9eb6
Hugo ARNAL <hugo.arnal@epitech.eu>
2025-05-02 15:42:59 +0200

    feat: remove useless pointer

--- src/vm.c
+++ src/vm.c
@@ -8,16 +8,16 @@
 #include "corewar.h"
 
 int add_to_map_and_champ(unsigned char map[MEM_SIZE],
-    flag_prog_t *champ, int champ_s, champion_t *champion)
+    flag_prog_t champ, int champ_s, champion_t *champion)
 {
     struct stat info;
 
-    fseek(champ->fp, 0, SEEK_SET);
-    stat(champ->prog_name, &info);
-    fread(&(champion->header), sizeof(header_t), 1, champ->fp);
+    fseek(champ.fp, 0, SEEK_SET);
+    stat(champ.prog_name, &info);
+    fread(&(champion->header), sizeof(header_t), 1, champ.fp);
     champion->procs[0].index = champ_s;
     fread(&map[champ_s], sizeof(char), info.st_size - sizeof(header_t),
-    champ->fp);
+        champ.fp);
     return 0;
 }
 
@@ -40,7 +40,7 @@ int do_vm(flags_t *champions)
     for (int i = 0; i < MEM_SIZE; i++)
         vm[i] = 0;
     for (int i = 0; champions->champions[i].active == true; i++) {
-        add_to_map_and_champ(vm, &(champions->champions[i]), index, champ[i]);
+        add_to_map_and_champ(vm, champions->champions[i], index, champ[i]);
         index += MEM_SIZE / champions->champions_amt;
     }
     gameloop(vm, champ);
