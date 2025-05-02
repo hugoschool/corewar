bba40c7e14a76191616114bcf57dc7422de011a6
Esteban Hazanas <esteban.hazanas@epitech.eu>
2025-05-02 19:03:54 +0200

    fix: zjump

--- src/instructions/zjump.c
+++ src/instructions/zjump.c
@@ -7,12 +7,13 @@
 
 #include "corewar.h"
 
-int do_zjump(char map[MEM_SIZE], champion_t *champ, int proc_index)
+int do_zjump(unsigned char map[MEM_SIZE], champion_t *champ, int proc_index)
 {
-    int nb1 = map[champ->procs[proc_index].index + 1];
-    int nb2 = map[champ->procs[proc_index].index + 2];
-    int nb3 = nb1 +nb2;
+    uint8_t nb1 = map[champ->procs[proc_index].index + 1];
+    uint8_t nb2 = map[champ->procs[proc_index].index + 2];
+    int16_t nb3 = (nb1 << 8) + (nb2);
 
+    nb3 = nb3 % IDX_MOD;
     champ->procs[proc_index].index += nb3;
     return 0;
 }
