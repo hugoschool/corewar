ad17f21b5a33b741cedb2a4dfb95e6978d5a115c
Hugo ARNAL <hugo.arnal@epitech.eu>
2025-05-06 10:52:09 +0200

    feat: add and sub

--- src/instructions/add.c
+++ src/instructions/add.c
@@ -7,14 +7,33 @@
 
 #include "corewar.h"
 
+static void add_or_sub(process_t *proc, unsigned char map[MEM_SIZE],
+    int index, bool sub)
+{
+    int first_register = proc->registers[map[index]];
+    int second_register = proc->registers[map[index + 1]];
+
+    if (!sub)
+        proc->registers[map[index + 2]] = first_register + second_register;
+    else
+        proc->registers[map[index + 2]] = first_register - second_register;
+}
+
 int do_add(unsigned char map[MEM_SIZE], champion_t *champ, int proc_index)
 {
+    int index = champ->procs[proc_index].index + 2;
+
+    add_or_sub(&champ->procs[proc_index], map, index, false);
     champ->procs[proc_index].index += 5;
     return 0;
 }
 
 int do_sub(unsigned char map[MEM_SIZE], champion_t *champ, int proc_index)
 {
+    int index = champ->procs[proc_index].index + 2;
+
+    add_or_sub(&champ->procs[proc_index], map, index, true);
+    printf("r%d: %d\n", map[index + 2], champ->procs[proc_index].registers[map[index + 2]]);
     champ->procs[proc_index].index += 5;
     return 0;
 }
