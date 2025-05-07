607559d084db4882e6cbee7e566ccd59e362ba16
Hugo ARNAL <hugo.arnal@epitech.eu>
2025-05-07 10:55:54 +0200

    imp: readability of and, or & xor

--- src/instructions/and.c
+++ src/instructions/and.c
@@ -32,7 +32,23 @@ static int value_of_type(process_t *proc, args_type_t type,
     return value;
 }
 
-int do_and(unsigned char map[MEM_SIZE], champion_t *champ, int proc_index)
+static int bit_and(int value, int cur_val)
+{
+    return value & cur_val;
+}
+
+static int bit_or(int value, int cur_val)
+{
+    return value | cur_val;
+}
+
+static int bit_xor(int value, int cur_val)
+{
+    return value ^ cur_val;
+}
+
+static void main_logic(unsigned char map[MEM_SIZE], champion_t *champ,
+    int proc_index, int (*bit_operation)(int value, int cur_val))
 {
     int index = champ->procs[proc_index].index + 1;
     args_type_t *types = byte_to_args(map[index]);
@@ -47,56 +63,27 @@ int do_and(unsigned char map[MEM_SIZE], champion_t *champ, int proc_index)
         if (value == 0)
             value = cur_val;
         else
-            value = value & cur_val;
+            value = (*bit_operation)(value, cur_val);
     }
     champ->procs[proc_index].registers[map[index] - 1] = value;
     champ->procs[proc_index].index += index_nb_bytes(types);
     free(types);
+}
+
+int do_and(unsigned char map[MEM_SIZE], champion_t *champ, int proc_index)
+{
+    main_logic(map, champ, proc_index, &bit_and);
     return 0;
 }
 
 int do_or(unsigned char map[MEM_SIZE], champion_t *champ, int proc_index)
 {
-    int index = champ->procs[proc_index].index + 1;
-    args_type_t *types = byte_to_args(map[index]);
-    int value = 0;
-    int cur_val = 0;
-
-    index++;
-    for (int i = 0; i < 2; i++) {
-        cur_val = value_of_type(&(champ->procs[proc_index]), types[i],
-            map, index);
-        index += get_nb_bytes(types[i]);
-        if (value == 0)
-            value = cur_val;
-        else
-            value = value | cur_val;
-    }
-    champ->procs[proc_index].registers[map[index] - 1] = value;
-    champ->procs[proc_index].index += index_nb_bytes(types);
-    free(types);
+    main_logic(map, champ, proc_index, &bit_or);
     return 0;
 }
 
 int do_xor(unsigned char map[MEM_SIZE], champion_t *champ, int proc_index)
 {
-    int index = champ->procs[proc_index].index + 1;
-    args_type_t *types = byte_to_args(map[index]);
-    int value = 0;
-    int cur_val = 0;
-
-    index++;
-    for (int i = 0; i < 2; i++) {
-        cur_val = value_of_type(&(champ->procs[proc_index]), types[i],
-            map, index);
-        index += get_nb_bytes(types[i]);
-        if (value == 0)
-            value = cur_val;
-        else
-            value = value ^ cur_val;
-    }
-    champ->procs[proc_index].registers[map[index] - 1] = value;
-    champ->procs[proc_index].index += index_nb_bytes(types);
-    free(types);
+    main_logic(map, champ, proc_index, &bit_xor);
     return 0;
 }
