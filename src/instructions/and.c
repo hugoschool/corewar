5d30e543bf19360cf6b02694ae9f5e5a1266426a
Hugo ARNAL <hugo.arnal@epitech.eu>
2025-05-07 10:44:40 +0200

    feat: indirect value & update using `get_nb_bytes`

--- src/instructions/and.c
+++ src/instructions/and.c
@@ -18,20 +18,18 @@ static int index_nb_bytes(args_type_t *types)
 }
 
 static int value_of_type(process_t *proc, args_type_t type,
-    unsigned char map[MEM_SIZE], int *index)
+    unsigned char map[MEM_SIZE], int index)
 {
-    int cur_val = 0;
+    int value = 0;
 
-    if (type == 1) {
-        cur_val = proc->registers[map[*index] - 1];
-        *index = *index + 1;
-    }
-    if (type == 2) {
-        cur_val = (map[*index] << 24) + (map[*index + 1] << 16) +
-            (map[*index + 2] << 8) + map[*index + 3];
-        *index = *index + 4;
-    }
-    return cur_val;
+    if (type == 1)
+        value = proc->registers[map[index] - 1];
+    if (type == 2)
+        value = (map[index] << 24) + (map[index + 1] << 16) +
+            (map[index + 2] << 8) + map[index + 3];
+    if (type == 3)
+        value = (map[index] << 8) + map[index + 1];
+    return value;
 }
 
 int do_and(unsigned char map[MEM_SIZE], champion_t *champ, int proc_index)
@@ -44,7 +42,8 @@ int do_and(unsigned char map[MEM_SIZE], champion_t *champ, int proc_index)
     index++;
     for (int i = 0; i < 2; i++) {
         cur_val = value_of_type(&(champ->procs[proc_index]), types[i],
-            map, &index);
+            map, index);
+        index += get_nb_bytes(types[i]);
         if (value == 0)
             value = cur_val;
         else
@@ -66,7 +65,8 @@ int do_or(unsigned char map[MEM_SIZE], champion_t *champ, int proc_index)
     index++;
     for (int i = 0; i < 2; i++) {
         cur_val = value_of_type(&(champ->procs[proc_index]), types[i],
-            map, &index);
+            map, index);
+        index += get_nb_bytes(types[i]);
         if (value == 0)
             value = cur_val;
         else
@@ -88,7 +88,8 @@ int do_xor(unsigned char map[MEM_SIZE], champion_t *champ, int proc_index)
     index++;
     for (int i = 0; i < 2; i++) {
         cur_val = value_of_type(&(champ->procs[proc_index]), types[i],
-            map, &index);
+            map, index);
+        index += get_nb_bytes(types[i]);
         if (value == 0)
             value = cur_val;
         else
