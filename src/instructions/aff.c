b34c87f083c490d3b5548a9c89e2370ae6341f61
Esteban Hazanas <esteban.hazanas@epitech.eu>
2025-05-07 10:42:43 +0200

    feat: aff

--- src/instructions/aff.c
+++ src/instructions/aff.c
@@ -9,6 +9,11 @@
 
 int do_aff(unsigned char map[MEM_SIZE], champion_t *champ, int proc_index)
 {
+    int index = champ->procs[proc_index].index;
+    int val = map[index + 2] < REG_NUMBER ?
+        champ->procs[proc_index].registers[map[index + 2]] : 0;
+
+    mini_printf("%c\n", val % 256);
     champ->procs[proc_index].index += 3;
     return 0;
 }
