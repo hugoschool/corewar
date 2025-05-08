c0e6529deaea588660f661778d021eaa39ee7088
Esteban Hazanas <esteban.hazanas@epitech.eu>
2025-05-08 15:28:59 +0200

    feat: st

--- src/instructions/st.c
+++ src/instructions/st.c
@@ -7,8 +7,30 @@
 
 #include "corewar.h"
 
+int rep_map(int val, unsigned char map[MEM_SIZE], int index)
+{
+    map[index] = (val << 24);
+    map[index + 1] = val << 16;
+    map[index + 2] = val << 8;
+    map[index + 3] = val << 0;
+}
+
 int do_st(unsigned char map[MEM_SIZE], champion_t *champ, int proc_index)
 {
+    int index = champ->procs[proc_index].index;
+    args_type_t *types = byte_to_args(map[index + 1]);
+    int val = 0;
+
+    if (types[1] == 1) {
+        val = champ->procs->registers[map[index + 2] - 1];
+        champ->procs[proc_index].registers[map[index + 3] - 1] = val;
+    }
+    if (types[1] == 3) {
+        val = (map[index + 3] << 8) + map[index + 4];
+        rep_map(champ->procs[proc_index].registers[map[index + 2] - 1],
+            map, index + (val % IDX_MOD));
+    }
     champ->procs[proc_index].index += 7;
+    free(types);
     return 0;
 }
